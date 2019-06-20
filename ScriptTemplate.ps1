d8fd8b09ba685f79bbc8700fe244a62f18f65282
Eric Austin <eaustin@gnresound.com>
2019-06-20 13:22:43 -0500

    Added date formats and parameter examples

--- ScriptTemplate.ps1
+++ ScriptTemplate.ps1
@@ -6,11 +6,16 @@
 ####################################
 
 using namespace System.Data
+using namespace System.Data.SqlClient
 
 $CurrentDirectory=if ($PSScriptRoot -ne "") {$PSScriptRoot} else {(Get-Location).Path}
 $ErrorActionPreference="Stop"
 $ErrorData=@()
 $ErrorLogLocation="$CurrentDirectory\ErrorLog.csv"
+
+$Date=(Get-Date).ToString() #returns "6/20/2019 9:10:21 AM" for use in log entries
+$Date=(Get-Date).ToString("yyyyMMdd") #returns "20190620" for use in file or folder names
+$Date=(Get-Date).ToString("yyyy-MM-dd HH:mm:ss") #returns "2019-06-20 09:16:41" for use in SQL Server
 
 #SQL Server parameters if needed (may also be populated from an external file for security)
 #use the .Net method of interacting with SQL Server even though it's more verbose because it's the only safe way to avoid SQL injection
@@ -27,8 +32,9 @@ Try {
     
     $Connection.Open()
     $Cmd=$Connection.CreateCommand()
-    $Cmd.CommandText="INSERT INTO VAPurchaseOrders_Staging (XMLText) VALUES (@value)"
-    $Cmd.Parameters.Add("@value", [SqlDbType]::Varchar,-1).Value=$Data
+    $Cmd.CommandText="INSERT INTO TaskSchedulerLog (TaskName, NextRunTime) VALUES (@TaskName, @NextRunTime)"
+    $Cmd.Parameters.Add("@TaskName", [SqlDbType]::VarChar,1000).Value=$_.TaskName
+    $Cmd.Parameters.Add("@NextRunTime", [SqlDbType]::DateTime).Value=if (($_."Next Run Time" -eq "N/A") -or ($_."Next Run Time" -eq "11/30/1999 12:00:00 AM")) {[System.DBNull]::Value} else {$_."Next Run Time"}
     $Cmd.ExecuteNonQuery() | Out-Null
 
 }
