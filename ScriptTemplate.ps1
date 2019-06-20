3776677b9f82f5f9d1be9d516565034a51b52e18
Eric Austin <eaustin@gnresound.com>
2019-06-20 08:32:17 -0500

    Revised namespace usage

--- ScriptTemplate.ps1
+++ ScriptTemplate.ps1
@@ -5,6 +5,7 @@
 # Description: This is a script template. It returns an exit code of -99 for use with Task Scheduler (to be used with Exec_PSScript.ps1) and creates an error log in the directory it is run out of.
 ####################################
 
+using namespace System.Data
 using namespace System.Data.SqlClient
 
 $CurrentDirectory=if ($PSScriptRoot -ne "") {$PSScriptRoot} else {(Get-Location).Path}
@@ -18,8 +19,9 @@ $Server=""
 $Database=""
 $Username=""
 $Password=''
-$Connection=New-Object System.Data.SqlClient.SqlConnection
-$ConnectionString="Server=$Server;Database=$Database;User Id=$Username;Password=$Password"
+$Connection=New-Object SqlConnection
+$ConnectionString="Server=$Server;Database=$Database;User Id=$Username;Password=$Password;"
+$ConnectionString="Server=$Server;Database=$Database;Trusted_Connection=True;"
 $Connection.ConnectionString=$ConnectionString
 
 Try {
@@ -27,8 +29,7 @@ Try {
     $Connection.Open()
     $Cmd=$Connection.CreateCommand()
     $Cmd.CommandText="INSERT INTO VAPurchaseOrders_Staging (XMLText) VALUES (@value)"
-    $Cmd.CommandTimeout=120
-    $Cmd.Parameters.Add("@value", [System.Data.SqlDbType]::Varchar, '-1').Value=$Data
+    $Cmd.Parameters.Add("@value", [SqlDbType]::Varchar,-1).Value=$Data
     $Cmd.ExecuteNonQuery() | Out-Null
 
 }
