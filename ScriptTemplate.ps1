efc3ddef9a51be2f3baa8de3af91f05b122fd05e
Eric Austin <eaustin@gnresound.com>
2020-04-13 11:30:42 -0500

    Updated

--- ScriptTemplate.ps1
+++ ScriptTemplate.ps1
@@ -4,71 +4,75 @@
 # Description:  This is a script template. It returns an exit code of 99 on error and creates an error log in the directory it is run out of.
 ####################################
 
-#declare namespaces
-using namespace System.Data
-using namespace System.Data.SqlClient
+#namespaces
+using namespace System.Data     #required for DataTable
+using namespace System.Data.SqlClient   
 using namespace System.Data.SQLite
 
-#set common variables
-$CurrentDirectory=if ($PSScriptRoot -ne "") {$PSScriptRoot} else {(Get-Location).Path}
-$ErrorActionPreference="Stop"
-$ErrorData=@()
-$ErrorLogLocation="$CurrentDirectory\ErrorLog.csv"
-
-#files to import
-$FilesToImportDirectory=""
-$AlreadyImportedDirectory=""
-
-#dot source function
-. "$CurrentDirectory\FunctionTemplate.ps1"
-
-#add SQLite assembly
-Add-Type -Path "$CurrentDirectory\System.Data.SQLite\System.Data.SQLite.dll"
-
-#common date variables
-$Date=(Get-Date).ToString() #returns "6/20/2019 9:10:21 AM" for use in log entries
-$Date=(Get-Date).ToString("yyyyMMdd") #returns "20190620" for use in file or folder names
-$Date=(Get-Date).ToString("yyyy-MM-dd HH:mm:ss") #returns "2019-06-20 09:16:41" for use in SQL Server
-
-#SQL Server parameters (may also be populated from an external file for security)
-#use the .Net method of interacting with SQL Server even though it's more verbose because it's the safe way to avoid SQL injection
-$Server=""
-$Database=""
-$Username=""
-$Password=''
-$Connection=New-Object SqlConnection
-$ConnectionString="Server=$Server;Database=$Database;User Id=$Username;Password=$Password;"
-$ConnectionString="Server=$Server;Database=$Database;Trusted_Connection=True;"
-$Connection.ConnectionString=$ConnectionString
-$Cmd=$Connection.CreateCommand()
-$Cmd.CommandTimeout=1000 #number of time in seconds to wait for the command to execute, default is 30 seconds
-
-#initialize SQL Server SqlBulkCopy object and parameters
-$Bulk=New-Object SqlBulkCopy($Connection)
-$Bulk.BatchSize=10000   #default batch size is 1
-$Bulk.DestinationTableName="Orders"
-
-#initialize datatable for use with SqlBulkCopy
-$Datatable=New-Object DataTable
-$Datatable.Columns.Add("RowCreateDate", "Datetime") | Out-Null
-$Datatable.Columns.Add("OrderYear", "Int16") | Out-Null
-$Datatable.Columns.Add("OrderMonth", "String") | Out-Null
-
-#map SqlBulkCopy datatable columns to SQL table columns
-$Bulk.ColumnMappings.Add("RowCreateDate", "RowCreateDate") | Out-Null
-$Bulk.ColumnMappings.Add("OrderYear", "OrderYear") | Out-Null
-$Bulk.ColumnMappings.Add("OrderMonth", "OrderMonth") | Out-Null
-
-#SQLite parameters
-$SLConnection=New-Object SQLiteConnection
-$SLConnectionString="Data Source=$CurrentDirectory\SQLite.db"
-$SLConnection.ConnectionString=$SLConnectionString
-$SLCmd=$SLConnection.CreateCommand()
-
-#initialize array for function splatting
-$DataForFunction=@()
-
 Try {
+
+    #begin variables
+
+    #common variables
+    $CurrentDirectory=[string]::IsNullOrWhiteSpace($PSScriptRoot) ? (Get-Location).Path : $PSScriptRoot
+    $ErrorActionPreference="Stop"
+    $ErrorData=@()
+    $ErrorLogLocation="$CurrentDirectory\ErrorLog.csv"
+    $ErrorLogLocation=(Join-Path -Path $CurrentDirectory -ChildPath "ErrorLog.csv")
+
+    #files to import
+    $FilesToImportDirectory=""
+    $AlreadyImportedDirectory=""
+
+    #dot source function
+    . "$CurrentDirectory\FunctionTemplate.ps1"
+
+    #add SQLite assembly
+    Add-Type -Path "$CurrentDirectory\System.Data.SQLite\System.Data.SQLite.dll"
+
+    #date variables
+    $Date=(Get-Date).ToString() #returns "6/20/2019 9:10:21 AM" for use in log entries
+    $Date=(Get-Date).ToString("yyyyMMdd") #returns "20190620" for use in file or folder names
+    $Date=(Get-Date).ToString("yyyy-MM-dd HH:mm:ss") #returns "2019-06-20 09:16:41" for use in SQL Server
+
+    #SQL Server parameters (may also be populated from an external sources for security)
+    $Server=""
+    $Database=""
+    $Username=""
+    $Password=''
+    $Connection=New-Object SqlConnection
+    $ConnectionString="Server=$Server;Database=$Database;User Id=$Username;Password=$Password;"
+    $ConnectionString="Server=$Server;Database=$Database;Trusted_Connection=True;"
+    $Connection.ConnectionString=$ConnectionString
+    $Cmd=$Connection.CreateCommand()
+    $Cmd.CommandTimeout=1000    #number of time in seconds to wait for the command to execute, default is 30 seconds
+
+    #initialize SQL Server SqlBulkCopy object and parameters
+    $Bulk=New-Object SqlBulkCopy($Connection)
+    $Bulk.BatchSize=10000   #default batch size is 1
+    $Bulk.DestinationTableName="Orders"
+
+    #initialize datatable for use with SqlBulkCopy
+    $Datatable=New-Object DataTable
+    $Datatable.Columns.Add("RowCreateDate", "Datetime") | Out-Null
+    $Datatable.Columns.Add("OrderYear", "Int16") | Out-Null
+    $Datatable.Columns.Add("OrderMonth", "String") | Out-Null
+
+    #map SqlBulkCopy datatable columns to SQL table columns
+    $Bulk.ColumnMappings.Add("RowCreateDate", "RowCreateDate") | Out-Null
+    $Bulk.ColumnMappings.Add("OrderYear", "OrderYear") | Out-Null
+    $Bulk.ColumnMappings.Add("OrderMonth", "OrderMonth") | Out-Null
+
+    #SQLite parameters
+    $SLConnection=New-Object SQLiteConnection
+    $SLConnectionString="Data Source=$CurrentDirectory\SQLite.db"
+    $SLConnection.ConnectionString=$SLConnectionString
+    $SLCmd=$SLConnection.CreateCommand()
+
+    #initialize array for function splatting
+    $DataForFunction=@()
+
+    #begin script
     
     $Connection.Open()
 
@@ -127,7 +131,7 @@ Catch {
     $ErrorData | Select-Object Date,ErrorMessage | Export-Csv -Path $ErrorLogLocation -Append -NoTypeInformation
 
     #return value
-    Exit 99
+    Exit 1
     
 }
 
